02d6562dfb55b365192e9ef8beea1960281254d7
FlowingFire <491643528@qq.com>
2017-10-15 20:22:20 +0800

    添加了日志属性

--- src/main/java/com/meeting/commands/CommandIn.java
+++ src/main/java/com/meeting/commands/CommandIn.java
@@ -1,5 +1,9 @@
 package com.meeting.commands;
 
+import com.meeting.utils.JSonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * User: FlowingFire
  * Date: 2017/10/15 0015
@@ -7,6 +11,8 @@ package com.meeting.commands;
  * Description:命令接口
  */
 public interface CommandIn {
+    public final static Logger logger = LoggerFactory.getLogger(CommandIn.class);
+
     public void simpleHelp();
     public boolean getOptions(String args[]);
     public void excute();
